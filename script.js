9aab3c11bd3378168b27bf3c60c00a2bb9072a80
mahi mj <maede.majnoon96@gmail.com>
2025-03-30 23:18:30 +0330

    trello_works-correctly

--- script.js
+++ script.js
@@ -1,27 +1,67 @@
-let input = document.querySelector("#task-input"); //input
-let button = document.querySelector("#add-task-btn"); //button
-let trello_ul = document.querySelector(".task-list"); //each ul in each columns
+let addTaskBtn = document.querySelector("#add-task-btn");
+let taskInput = document.querySelector("#task-input");
+let columns = document.querySelector(".columns");
+let eachColumns = document.querySelectorAll(".column");
+let toDo = document.querySelector("#todo");
 
-button.addEventListener("click", () => {
-  let new_task = input.value;
+let draggedTask = null;
+
+addTaskBtn.addEventListener("click", function (e) {
+  let new_task = taskInput.value;
   if (new_task == "") return;
-  let li = document.createElement("li");
-  li.innerHTML = `
-  <div class="task draggable">
-  <span>${new_task}</span>
-  </div>`;
-  trello_ul.appendChild(li);
-  input.value = "";
-  let task = document.querySelectorAll(".draggable");
-  //console.log(task);
-  task.forEach((t) => {
-    //console.log(t.textContent);
-    t.setAttribute("draggable", true);
-    t.addEventListener("dragstart", () => {});
-    t.addEventListener("dragenter", () => {});
-    t.addEventListener("dragover", () => {});
-    t.addEventListener("dragleave", () => {});
-    t.addEventListener("drop", () => {});
-    t.addEventListener("dragend", () => {});
+  let div = document.createElement("div");
+  div.innerHTML = `<div class="task draggable"><p>${new_task}</p></div>`;
+  toDo.appendChild(div);
+  taskInput.value = "";
+  //eachColumns.forEach((col) => {
+  document.querySelectorAll(".task").forEach((task) => {
+    task.setAttribute("draggable", true);
+    task.addEventListener("dragstart", handleDragStart);
+    // task.addEventListener("dragenter", handleDragEnter);
+    // task.addEventListener("dragover", handleDragOver);
+    // task.addEventListener("dragleave", handleDragLeave);
+    // task.addEventListener("drop", handleDrop);
+    task.addEventListener("dragend", handleDragEnd);
+  });
+  document.querySelectorAll(".column").forEach((col) => {
+    col.addEventListener("dragenter", handleDragEnter);
+    col.addEventListener("dragover", handleDragOver);
+    col.addEventListener("dragleave", handleDragLeave);
+    col.addEventListener("drop", handleDrop);
   });
 });
+function handleDragStart(e) {
+  draggedTask = this;
+  console.log("start");
+  //e.target.classList.add("drag");
+  e.dataTransfer.setData("text/html", e.target.outerHTML);
+  //console.log(e.target);
+  console.log("over");
+  e.target.classList.add("over");
+}
+function handleDragEnter(e) {
+  console.log("enter");
+}
+function handleDragOver(e) {
+  if (e.preventDefault()) e.preventDefault();
+  e.target.classList.add("dragging");
+}
+function handleDragLeave(e) {
+  console.log("leave");
+  e.target.classList.remove("dragging");
+}
+function handleDrop(e) {
+  console.log("drop");
+  //console.log(e.dataTransfer.getData("text/html"));
+  //console.log(e.target);
+  let col2 = document.querySelector("#in-progress");
+  //e.target.appendChild(col2);
+  if (draggedTask) {
+    this.appendChild(draggedTask);
+  }
+  e.target.classList.remove("dragging");
+}
+function handleDragEnd(e) {
+  console.log("end");
+  e.target.classList.remove("over");
+}
